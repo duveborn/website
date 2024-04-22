fb6511276d2585b8b145fdae54e27f8ed159b740
Oskar Duveborn <weyland@flowersfx.com>
2024-04-22 15:02:07 +0200

    quick hack with some stuff

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24">
+      <div className="relative flex place-items-center">
         <Image
           src="/avatar.png"
           alt="Oskar Duveborn"
@@ -13,18 +13,74 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+      <div className="my-8 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://www.linkedin.com/in/duveborn/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://www.syntax-error.se/"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            LinkedIn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`text-teal-300 sm:mb-3 text-2xl font-semibold`}>
+            Syntax Error
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            I&apos;m an organizer, the treasurer and a DJ at the hon-profit nightclub in Sweden promoting video game music and a friendly environment.
+          </p>
+        </a>
+
+        <a
+          href="https://www.sec-t.org/"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`text-teal-300 sm:mb-3 text-2xl font-semibold`}>
+            SEC-T
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            I&apos;m the Club Manager for the large cyber security conference SEC-T in Stockholm.
+          </p>
+        </a>
+
+        <a
+          href="https://mandagsklubben.net/"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`text-teal-300 sm:mb-3 text-2xl font-semibold`}>
+            Måndagsklubben
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            I&apos;m a founder and an organizer of the weekly tech afterwork in Stockholm called Måndagsklubben.
+          </p>
+        </a>
+
+        <a
+          href="https://fuzzycats.se"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`text-teal-300 sm:mb-3 text-2xl font-semibold`}>
+            Fuzzycats
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            I&apos;m the DJ in the DJ+VJ duo Fuzzycats who believes Big Beat is ripe for a comeback.
+          </p>
+        </a>
+      </div>
+
+      <div className="my-8 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://www.linkedin.com/in/duveborn/"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`sm:mb-3 text-2xl font-semibold`}>
+            LinkedIn
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Contact me on LinkedIn for collaborations and other ideas.
@@ -33,15 +89,12 @@ export default function Home() {
 
         <a
           href="https://github.com/duveborn"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Github{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`sm:mb-3 text-2xl font-semibold`}>
+            Github
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Check out my public activity on Github.
@@ -50,15 +103,12 @@ export default function Home() {
 
         <a
           href="https://www.instagram.com/oskarduveborn/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Instagram{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`sm:mb-3 text-2xl font-semibold`}>
+            Instagram
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Enjoy my casual photo vlogging on Instagram.
@@ -67,15 +117,12 @@ export default function Home() {
 
         <a
           href="https://twitter.com/OskarDuveborn"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group px-5 py-4 transition-colors hover:border-gray-300 hover:bg-cyan-100 hover:dark:border-neutral-700 hover:dark:bg-cyan-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            X{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className={`sm:mb-3 text-2xl font-semibold`}>
+            X
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Message me on Twitter for collaborations or other ideas.
